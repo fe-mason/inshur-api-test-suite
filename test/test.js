db52f4caa6e6fcea4f02c968862e549d55907017
Fe <fe@ottewhal.com>
2020-07-03 18:02:07 +0100

    adds invalid productId test

--- test/test.js
+++ test/test.js
@@ -7,27 +7,28 @@ const allProducts = '/products';
 const customers = '/customers';
 const individualProduct = '/product/';
 const taxiProductId = '1234';
+const invalidProductId = '2222';
 
-/***Scenario:Retrieve all products in the INSHUR app
-    Given: The products "Taxi" and "courier" are available
-    When: The request is made to retrieve all products
-    Then: the products "Taxi" and "courier" will be retrieved***/
+describe('inshur-products', function(){
 
-describe('inshur-app', function(){
+    /***Scenario:Retrieve all products in the INSHUR app
+        Given: The products "Taxi" and "courier" are available
+        When: The request is made to retrieve all products
+        Then: the products "Taxi" and "courier" will be retrieved***/
 
-  it('retrieves all available products', (done) => {
-      chai.request(baseUrl)
-        .get(allProducts)
-        .then(function (res) {
-            const products = res.body["products"];
-            const productMap = products.map(product => product.productName)
+    it('retrieves all available products', (done) => {
+        chai.request(baseUrl)
+            .get(allProducts)
+            .then(function (res) {
+                const products = res.body["products"];
+                const productMap = products.map(product => product.productName)
 
-            expect(productMap).to.include('Taxi Product');
-            expect(productMap).to.include('Courier Product');
-            done();
-        })
-        .catch(done);
-  });
+                expect(productMap).to.include('Taxi Product');
+                expect(productMap).to.include('Courier Product');
+                done();
+            })
+            .catch(done);
+    });
 
     /***Scenario:Retrieve individual product
         Given: The product "Taxi" which has a productId of "1234"
@@ -45,22 +46,20 @@ describe('inshur-app', function(){
         .catch(done);
   });
 
+    /***Scenario:Retrieve non existent product
+    Given: A product with producId "2222" does not existent
+    When: The request is made to retrieve this product
+    Then: And error is thrown "product 2222 not found"***/
+
+    it('handles a non-existent product', (done) => {
+      chai.request(baseUrl)
+        .get(`${individualProduct}/${invalidProductId}`)
+        .then(function (res) {
+            expect(res).to.have.status(404);
+            expect(res.body).to.have.property('errorMessage', "product 2222 not found");
+            done();
+        })
+        .catch(done);
+  });
+
 });
-
-/***Scenario:Retrieve individual product
-Given: The product "Taxi" which has a productId of "1234"
-When: The request is made to retrieve this product
-Then: The product version "1.0" of "Taxi" is available
-
-
-Scenario:Retrieve non existent product
-Given: A product with producId "2222" does not existent
-When: The request is made to retrieve this product
-Then: And error is thrown "product 2222 not found"
-
-Feature2: Customers
-Scenairo: Retreive all existing customers
-Given: the following customers are available       
-| customer1 | customer2 | customer3 | customer4| customer5 | customer6 |
-When: The request is made to retrieve all customers
-Then: all customer details are retrieved successfully***/
