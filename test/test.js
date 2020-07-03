50b5f536c8156310473ddf220ff2a682b27c6598
Fe <fe@ottewhal.com>
2020-07-03 14:57:01 +0100

    adds initial test setup

---
+++ test/test.js
@@ -0,0 +1,47 @@
+const chai = require('chai'), chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const baseUrl = 'http://simondfranklininshur.pythonanywhere.com';
+const allProducts = '/products';
+const customers = '/customers';
+const individualProduct = '/product/';
+const productId = '';
+
+/***Scenario:Retrieve all products in the INSHUR app
+    Given: The products "Taxi" and "courier" are available
+    When: The request is made to retrieve all products
+    Then: the products "Taxi" and "courier" will be retrieved***/
+
+describe('inshur-app', function(){
+
+  it('retrieves all available products', () => {
+      chai.request(baseUrl)
+        .get(allProducts)
+        .then(function (res) {
+            expect(res).to.have.status(200);
+        })
+        .catch(function (err) {
+            throw err;
+        });
+  });
+
+
+});
+
+/***Scenario:Retrieve individual product
+Given: The product "Taxi" which has a productId of "1234"
+When: The request is made to retrieve this product
+Then: The product version "1.0" of "Taxi" is available
+
+
+Scenario:Retrieve non existent product
+Given: A product with producId "2222" does not existent
+When: The request is made to retrieve this product
+Then: And error is thrown "product 2222 not found"
+
+Feature2: Customers
+Scenairo: Retreive all existing customers
+Given: the following customers are available       
+| customer1 | customer2 | customer3 | customer4| customer5 | customer6 |
+When: The request is made to retrieve all customers
+Then: all customer details are retrieved successfully***/
